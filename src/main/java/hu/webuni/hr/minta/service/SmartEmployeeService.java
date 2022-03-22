528fdc03e556889d69dc4e1476ae674097796525
imre-gabor <gabor@aut.bme.hu>
2022-03-22 18:59:33 +0100

    solution at live 1

--- src/main/java/hu/webuni/hr/minta/service/SmartEmployeeService.java
+++ src/main/java/hu/webuni/hr/minta/service/SmartEmployeeService.java
@@ -1,5 +1,6 @@
 package hu.webuni.hr.minta.service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Map.Entry;
@@ -22,19 +23,47 @@ public class SmartEmployeeService implements EmployeeService {
 	@Override
 	public int getPayRaisePercent(Employee employee) {
 		
+		//Duration.between(employee.getDateOfStartWork(), LocalDateTime.now()).toDays();
+
 		double yearsWorked = ChronoUnit.DAYS.between(employee.getDateOfStartWork(), LocalDateTime.now()) / 365.0;
 		
 		Smart smartConfig = config.getSalary().getSmart();
-		if(yearsWorked > smartConfig.getLimit3())
-			return smartConfig.getPercent3();
 		
-		if(yearsWorked > smartConfig.getLimit2())
-			return smartConfig.getPercent2();
+		//1. megoldás: fixen 3 limit és percent
+//		if(yearsWorked > smartConfig.getLimit3())
+//			return smartConfig.getPercent3();
+//		
+//		if(yearsWorked > smartConfig.getLimit2())
+//			return smartConfig.getPercent2();
+//		
+//		if(yearsWorked > smartConfig.getLimit1())
+//			return smartConfig.getPercent1();
+//		return 0;
 		
-		if(yearsWorked > smartConfig.getLimit1())
-			return smartConfig.getPercent1();
+		//2. megoldás: dinamikusan, akárhány limit és percent
+		TreeMap<Double, Integer> limits = smartConfig.getLimits();
 		
-		return 0;
+//		Integer maxPercent = null;
+//		
+//		for (Entry<Double, Integer> entry: limits.entrySet()) {
+//			if(yearsWorked > entry.getKey())
+//				maxPercent = entry.getValue();
+//			else
+//				break;
+//		}
+//
+//		return maxPercent == null ? 0 : maxPercent;
+		
+		//3. megoldás
+//		Optional<Double> optionalMax = limits.keySet().stream()
+//			.filter(k -> yearsWorked >= k)
+//			.max(Double::compare);
+//		
+//		return optionalMax.isEmpty() ? 0 : limits.get(optionalMax.get());
+		
+		//4. megoldás
+		Entry<Double, Integer> floorEntry = limits.floorEntry(yearsWorked);
+		return floorEntry == null ? 0 : floorEntry.getValue();
 	}
 
 }
